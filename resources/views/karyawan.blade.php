523e4c3fb7a34d64e0ca5e03b03513e1611bc74f
arsyadarmawan <thareeqarsyad@gmail.com>
2020-01-24 11:28:49 +0700

    view

--- resources/views/karyawan.blade.php
+++ resources/views/karyawan.blade.php
@@ -1,44 +1,127 @@
-{{-- @extends('layouts.global') --}}
-{{-- @extends('layouts.head') --}}
-{{-- @section('title')
-    DASHBOARD
-@endsection --}}
-{{-- @section('content') --}}
-    <div class="main-content">
-        <section class="section">
-        <div class="section-header">
-                    <div class="col-md-12">
-                {{-- <div class="row mb-3"></div> --}}
-                <br>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th class="text-center" scope="col"><b>NIK</b></th>
-                        <th class="text-center" scope="col"><b>Nama</b></th>
-                        <th class="text-center" scope="col"><b>Golongan</b></th>
-                        <th class="text-center" scope="col"><b>Jabatan</b></th>
-                        <th class="text-center" scope="col"><b>Gaji</b></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($data as $item)
-                        <tr>
-                            <td>{{$item->nik}}</td>  
-                            <td>{{$item->nama}}</td>
-                            <td>{{$item->golongan}}</td>
-                            <td>{{$item->jabatan}}</td>
-                            <td>{{$item->gaji}}</td>          
-                        </tr>                      
-                    @endforeach
+    <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                </tbody>
-            </table>
-        </div>
-        </div>
+        <title>Laravel</title>
 
-        <div class="section-body">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+
+            <div class="content">
+                <div class="main-content">
+                    <section class="section">
+                    <div class="section-header">
+                                <div class="col-md-12">
+                            {{-- <div class="row mb-3"></div> --}}
+                            <br>
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th class="text-center" scope="col"><b>NIK</b></th>
+                                    <th class="text-center" scope="col"><b>Nama</b></th>
+                                    <th class="text-center" scope="col"><b>Golongan</b></th>
+                                    <th class="text-center" scope="col"><b>Jabatan</b></th>
+                                    <th class="text-center" scope="col"><b>Gaji</b></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td>{{$item->nik}}</td>  
+                                        <td>{{$item->nama}}</td>
+                                        <td>{{$item->golongan}}</td>
+                                        <td>{{$item->jabatan}}</td>
+                                        <td>{{$item->gaji}}</td>          
+                                    </tr>                      
+                                @endforeach
+                
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
+                
+                    <div class="section-body">
+                    </div>
+                    </section>
+                </div>
+
+
+            </div>
         </div>
-        </section>
-    </div>
-{{-- @endsection --}}
-{{--  --}}
+    </body>
+</html>
+
+    
